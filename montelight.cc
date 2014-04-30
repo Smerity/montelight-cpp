ac3229646ea42945394ad4b8840d8e8e3de86a3d
Stephen Merity <smerity@smerity.com>
2014-04-30 18:09:56 -0400

    Extended to use Vectors for Circle intersect tests + Shape inheritance

--- montelight.cc
+++ montelight.cc
@@ -9,6 +9,12 @@ struct Vector {
   double x, y, z;
   //
   Vector(double x_=0, double y_=0, double z_=0) : x(x_), y(y_), z(z_) {}
+  inline Vector operator-(const Vector &o) const {
+    return Vector(x - o.x, y - o.y, z - o.z);
+  }
+  inline double dot(const Vector &o) const {
+    return x * o.x + y * o.y + z*o.z;
+  }
 };
 
 struct Image {
@@ -18,7 +24,7 @@ struct Image {
   Image(unsigned int w, unsigned int h) : width(w), height(h) {
     pixels = new Vector[width * height];
   }
-  void setPixel(unsigned int x, unsigned int y, Vector &v) {
+  void setPixel(unsigned int x, unsigned int y, const Vector &v) {
     pixels[y * width + x] = v;
   }
   void save(std::string filePrefix) {
@@ -37,19 +43,59 @@ struct Image {
   }
 };
 
+struct Shape {
+  Vector color;
+  //
+  Shape(const Vector color_) : color(color_) {}
+  virtual double intersects(const Vector &point) const { return 0; }
+};
+
+struct Circle : Shape {
+  Vector center, color;
+  double radius;
+  //
+  Circle(const Vector center_, double radius_, const Vector color_) :
+    Shape(color_), center(center_), radius(radius_) {}
+  double intersects(const Vector &point) const {
+    Vector d = point - center;
+    if (d.dot(d) < radius) {
+      return 1;
+    }
+    return 0;
+  }
+};
+
 int main(int argc, const char *argv[])
 {
-  Image img(256, 256);
-  for (int y = 0; y < 256; ++y) {
-    for (int x = 0; x < 256; ++x) {
-      auto blue = Vector(75, 200, 75);
-      int dx = x - 100, dy = y - 100;
-      int r = 100;
-      if (dx * dx + dy * dy < r) {
-        img.setPixel(x, y, blue);
+  // Initialize the image
+  int w = 256, h = 256;
+  Image img(w, h);
+  // Set up the scene
+  Shape *scene[] = {
+    new Circle(Vector(100, 40, 0), 100, Vector(75, 200, 75)), // Green
+    new Circle(Vector(40, 200, 0), 400, Vector(275, 75, 100)), // Red
+    new Circle(Vector(190, 170, 0), 300, Vector(125, 75, 300)) // Blue
+  };
+  // Set up the camera
+  // Take a set number of samples per pixel
+  for (int samples = 0; samples < 1; ++samples) {
+    // For each pixel, sample a ray in that direction
+    for (int y = 0; y < h; ++y) {
+      for (int x = 0; x < w; ++x) {
+        // Calculate the direction of the camera ray
+        // Check for intersection
+        Vector ray = Vector(x, y, 0);
+        for (auto obj : scene) {
+          if (obj->intersects(ray) > 0) {
+            //img.setPixel(x, y, Vector(75, 200, 75));
+            img.setPixel(x, y, obj->color);
+          }
+        }
+        // Add result of sample to image
       }
     }
   }
-  img.save("green");
+  // Save the resulting raytraced image
+  img.save("render");
   return 0;
 }
