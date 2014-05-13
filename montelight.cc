b0ef24d79664df256a716a4495e2e8bd91197296
Stephen Merity <smerity@smerity.com>
2014-05-12 22:17:11 -0400

    Rearranging code and improve the default generated image

--- montelight.cc
+++ montelight.cc
@@ -15,45 +15,6 @@ using namespace std;
 // Globals
 bool EMITTER_SAMPLING = true;
 
-// Set up our testing scenes
-// They're all Cornell box inspired: http://graphics.ucsd.edu/~henrik/images/cbox.html
-/////////////////////////
-// Scene format: Sphere(position, radius, color, emission)
-/////////////////////////
-std::vector<Shape *> simpleScene = {
-  // Left sphere
-  new Sphere(Vector(1e5+1,40.8,81.6), 1e5f, Vector(.75,.25,.25), Vector()),
-  // Right sphere
-  new Sphere(Vector(-1e5+99,40.8,81.6), 1e5f, Vector(.25,.25,.75), Vector()),
-  // Back sphere
-  new Sphere(Vector(50,40.8, 1e5), 1e5f, Vector(.75,.75,.75), Vector()),
-  // Floor sphere
-  new Sphere(Vector(50, 1e5, 81.6), 1e5f, Vector(.75,.75,.75), Vector()),
-  // Roof sphere
-  new Sphere(Vector(50,-1e5+81.6,81.6), 1e5f, Vector(.75,.75,.75), Vector()),
-  // Traditional mirror sphere
-  new Sphere(Vector(27,16.5,47), 16.5f, Vector(1,1,1) * 0.799, Vector()),
-  // Traditional glass sphere
-  new Sphere(Vector(73,16.5,78), 16.5f, Vector(1,1,1) * 0.799, Vector()),
-  // Light source
-  //new Sphere(Vector(50,681.6-.27,81.6), 600, Vector(1,1,1) * 0.5, Vector(12,12,12))
-  new Sphere(Vector(50,65.1,81.6), 8.5, Vector(), Vector(4,4,4) * 100) // Small = 1.5, Large = 8.5
-};
-/////////////////////////
-std::vector<Shape *> complexScene = {
-  new Sphere(Vector(1e5+1,40.8,81.6), 1e5f, Vector(.75,.25,.25), Vector()), // Left
-  new Sphere(Vector(-1e5+99,40.8,81.6), 1e5f, Vector(.25,.25,.75), Vector()), // Right
-  new Sphere(Vector(50,40.8, 1e5), 1e5f, Vector(.75,.75,.75), Vector()), // Back
-  new Sphere(Vector(50, 1e5, 81.6), 1e5f, Vector(.75,.75,.75), Vector()), //Bottom
-  new Sphere(Vector(50,-1e5+81.6,81.6), 1e5f, Vector(.75,.75,.75), Vector()), // Top
-  new Sphere(Vector(20,16.5,40), 16.5f, Vector(1,1,1) * 0.799, Vector()),
-  new Sphere(Vector(50,16.5,80), 16.5f, Vector(1,1,1) * 0.799, Vector()),
-  new Sphere(Vector(75,16.5,120), 16.5f, Vector(1,1,1) * 0.799, Vector()),
-  new Sphere(Vector(50,65.1,40), 1.5, Vector(), Vector(4,4,4) * 100), // Light
-  new Sphere(Vector(50,65.1,120), 1.5, Vector(), Vector(4,4,4) * 100), // Light
-};
-//
-
 struct Vector {
   double x, y, z;
   //
@@ -255,6 +216,45 @@ struct Sphere : Shape {
   }
 };
 
+// Set up our testing scenes
+// They're all Cornell box inspired: http://graphics.ucsd.edu/~henrik/images/cbox.html
+/////////////////////////
+// Scene format: Sphere(position, radius, color, emission)
+/////////////////////////
+std::vector<Shape *> simpleScene = {
+  // Left sphere
+  new Sphere(Vector(1e5+1,40.8,81.6), 1e5f, Vector(.75,.25,.25), Vector()),
+  // Right sphere
+  new Sphere(Vector(-1e5+99,40.8,81.6), 1e5f, Vector(.25,.25,.75), Vector()),
+  // Back sphere
+  new Sphere(Vector(50,40.8, 1e5), 1e5f, Vector(.75,.75,.75), Vector()),
+  // Floor sphere
+  new Sphere(Vector(50, 1e5, 81.6), 1e5f, Vector(.75,.75,.75), Vector()),
+  // Roof sphere
+  new Sphere(Vector(50,-1e5+81.6,81.6), 1e5f, Vector(.75,.75,.75), Vector()),
+  // Traditional mirror sphere
+  new Sphere(Vector(27,16.5,47), 16.5f, Vector(1,1,1) * 0.799, Vector()),
+  // Traditional glass sphere
+  new Sphere(Vector(73,16.5,78), 16.5f, Vector(1,1,1) * 0.799, Vector()),
+  // Light source
+  //new Sphere(Vector(50,681.6-.27,81.6), 600, Vector(1,1,1) * 0.5, Vector(12,12,12))
+  new Sphere(Vector(50,65.1,81.6), 8.5, Vector(), Vector(4,4,4) * 100) // Small = 1.5, Large = 8.5
+};
+/////////////////////////
+std::vector<Shape *> complexScene = {
+  new Sphere(Vector(1e5+1,40.8,81.6), 1e5f, Vector(.75,.25,.25), Vector()), // Left
+  new Sphere(Vector(-1e5+99,40.8,81.6), 1e5f, Vector(.25,.25,.75), Vector()), // Right
+  new Sphere(Vector(50,40.8, 1e5), 1e5f, Vector(.75,.75,.75), Vector()), // Back
+  new Sphere(Vector(50, 1e5, 81.6), 1e5f, Vector(.75,.75,.75), Vector()), //Bottom
+  new Sphere(Vector(50,-1e5+81.6,81.6), 1e5f, Vector(.75,.75,.75), Vector()), // Top
+  new Sphere(Vector(20,16.5,40), 16.5f, Vector(1,1,1) * 0.799, Vector()),
+  new Sphere(Vector(50,16.5,80), 16.5f, Vector(1,1,1) * 0.799, Vector()),
+  new Sphere(Vector(75,16.5,120), 16.5f, Vector(1,1,1) * 0.799, Vector()),
+  new Sphere(Vector(50,65.1,40), 1.5, Vector(), Vector(4,4,4) * 100), // Light
+  new Sphere(Vector(50,65.1,120), 1.5, Vector(), Vector(4,4,4) * 100), // Light
+};
+//
+
 struct Tracer {
   std::vector<Shape *> scene;
   //
@@ -341,11 +341,11 @@ int main(int argc, const char *argv[]) {
   // Variables to modify the process or the images
   EMITTER_SAMPLING = true;
   int w = 256, h = 256;
-  int SNAPSHOT_INTERVAL = 25;
-  unsigned int SAMPLES = 100;
-  bool FOCUS_EFFECT = true;
-  double FOCAL_LENGTH = 20;
-  double APERTURE_FACTOR = 0.5; // ratio of original/new aperture (>1: smaller view angle, <1: larger view angle)
+  int SNAPSHOT_INTERVAL = 10;
+  unsigned int SAMPLES = 50;
+  bool FOCUS_EFFECT = false;
+  double FOCAL_LENGTH = 35;
+  double APERTURE_FACTOR = 1; // ratio of original/new aperture (>1: smaller view angle, <1: larger view angle)
   // Initialize the image
   Image img(w, h);
   /////////////////////////
